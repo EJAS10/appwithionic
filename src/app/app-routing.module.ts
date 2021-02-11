852bff8426d0226e7f7eaa4427cf83cfb4b297e5
Emmanuel Almanzar <ea.emmanuel02@gmail.com>
2021-02-11 22:33:20 +0100

    cambios para la tarea de la semana 4

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,14 +1,14 @@
 import { NgModule } from '@angular/core';
 import { PreloadAllModules, RouterModule, Routes } from '@angular/router';
 
-const routes: Routes = [
+const routes: Routes = [  
+  {
+    path: 'app',
+    loadChildren: () => import('./tabs/tabs.module').then(m => m.TabsPageModule)
+  },   
   {
     path: '',
-    loadChildren: () => import('./tabs/tabs.module').then(m => m.TabsPageModule)
-  },
-  {
-    path: 'tab4',
-    loadChildren: () => import('./tab4/tab4.module').then( m => m.Tab4PageModule)
+    loadChildren: () => import('./login/login.module').then( m => m.LoginPageModule)
   }
 ];
 @NgModule({
---
+++ src/app/login/login.page.ts
@@ -0,0 +1,29 @@
+import { Component, OnInit } from '@angular/core';
+import { NavController } from '@ionic/angular';
+// import { AppRoutingModule } from './../app-routing.module';
+
+@Component({
+  selector: 'app-login',
+  templateUrl: './login.page.html',
+  styleUrls: ['./login.page.scss'],
+})
+export class LoginPage implements OnInit {
+
+  user: string;
+  pass: string;
+  Mensaje: boolean = false;
+  constructor(private navController: NavController) { 
+    
+  }
+  Singup(){
+     if(this.user == "usuario" && this.pass == "admin"){
+      this.navController.navigateRoot(`app/tabs/tab1`);
+     }    
+     else{
+       this.Mensaje = true;
+     }
+  }
+  ngOnInit() {
+  }
+
+}
